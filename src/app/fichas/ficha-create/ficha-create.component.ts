0be39736577db5ff2a4dbe04ddacb9b090701902
unknown <wallacecardosogomes@gmail.com>
2019-05-08 21:53:59 -0300

    adcionana pagina e component ficha avaliacao

---
+++ src/app/fichas/ficha-create/ficha-create.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-ficha-create',
+    templateUrl: './ficha-create.component.html'
+})
+export class FichaCreateComponent {
+
+}
