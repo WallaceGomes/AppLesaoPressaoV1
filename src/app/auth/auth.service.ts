545b80927958adc0a509187a48a86d2eaa57f3b2
unknown <wallacecardosogomes@gmail.com>
2019-05-22 21:33:18 -0300

    Final autenticação - está com um erro na parte do token que nao consegui identificar

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -8,6 +8,7 @@ import { Router } from '@angular/router';
 @Injectable({ providedIn: 'root' })
 export class AuthService {
     private token: string;
+    private tokenTimer: any;
     private authStatusListener = new Subject<boolean>();
     private isAutenticado = false;
 
@@ -35,21 +36,73 @@ export class AuthService {
 
     login(email: string, senha: string) {
         const authData: AuthData = {email: email, senha: senha}
-        this.http.post<{token: string}>('http://localhost:3000/api/usuario/login', authData)
+        this.http.post<{token: string, expiresIn: number }>('http://localhost:3000/api/usuario/login', authData)
         .subscribe(response => {
             const token = response.token;
             this.token = token;
             if (token) {
+                const expiresInDuration = response.expiresIn;
+                this.setAuthTimer(expiresInDuration);
                 this.isAutenticado = true;
                 this.authStatusListener.next(true);
+                const now = new Date();
+                const expirationDate = new Date(now.getTime() + expiresInDuration * 1000);
+                this.saveAuthData(token, expirationDate);
                 this.router.navigate(['/']);
             }
         });
     }
+
+    autoAuthUser() {
+        const authInformation = this.getAuthData();
+        if (!authInformation) {
+            return;
+        }
+        const now = new Date();
+        const expiresIn = authInformation.expirationDate.getTime() - now.getTime();
+        if (expiresIn > 0) {
+            this.token = authInformation.token;
+            this.isAutenticado = true;
+            this.setAuthTimer(expiresIn / 1000);
+            this.authStatusListener.next(true);
+        }
+    }
+
     logout() {
         this.token = null;
         this.isAutenticado = false;
         this.authStatusListener.next(false);
+        clearTimeout(this.tokenTimer);
+        this.clearAuthData();
         this.router.navigate(['/']);
+    }
+
+    private setAuthTimer(duration: number) {
+        console.log("Setting timer: " + duration);
+        this.tokenTimer = setTimeout(() => {
+            this.logout();
+        }, duration * 1000);
+    }
+
+    private saveAuthData(token: string, expirationDate: Date) {
+        localStorage.setItem('token', token);
+        localStorage.setItem('expiration', expirationDate.toISOString());
+    }
+
+    private clearAuthData() {
+        localStorage.removeItem('token');
+        localStorage.removeItem('expiration');
+    }
+
+    private getAuthData() {
+        const token = localStorage.getItem('token');
+        const expirationDate = localStorage.getItem('expiration');
+        if (!token || !expirationDate) {
+            return;
+        }
+        return {
+            token: token,
+            expirationDate: new Date(expirationDate)
+        }
     }
 }
